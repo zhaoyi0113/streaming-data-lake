c78e831452f46130ffa99c04e303c5bfff1ea14d
Joey Zhao <zhaoyi0113@gmail.com>
2019-08-08 17:02:16 +1000

    update build script

--- bin/build_lambdas.sh
+++ bin/build_lambdas.sh
@@ -2,5 +2,7 @@ BUILD_DIR=dist
 rm -fr $BUILD_DIR
 mkdir -p $BUILD_DIR
 cp -fr src/lambdas $BUILD_DIR
+cd $BUILD_DIR
 zip -r finance_producer.zip *
 aws s3 cp finance_producer.zip s3://${TF_VAR_S3_STREAMING_DATA}/lambdas/
+cd ..
