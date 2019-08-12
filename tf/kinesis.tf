3c36beef3185647cc449d46130e17cb3a9a426c0
Joey Zhao <zhaoyi0113@gmail.com>
2019-08-12 12:22:00 +1000

    add kinsis firehose

--- tf/kinesis.tf
+++ tf/kinesis.tf
@@ -3,3 +3,33 @@ resource "aws_kinesis_stream" "finance_stream" {
   shard_count      = 1
   retention_period = 24
 }
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose_finance_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
+  name        = "${var.project_name}-finance-stream"
+  destination = "s3"
+
+  s3_configuration {
+    role_arn   = "${aws_iam_role.firehose_role.arn}"
+    bucket_arn = "${aws_s3_bucket.target_bucket.arn}"
+  }
+}
